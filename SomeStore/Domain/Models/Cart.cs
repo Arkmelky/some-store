4d4eedf6678ea0fa727099e52fe82e08b686685b
Arkady Soloveyko <arkmelky@mail.ru>
2016-03-23 01:24:14 +0300

    Cart added Signed-off-by: Arkady Soloveyko <arkmelky@mail.ru>

---
+++ SomeStore/Domain/Models/Cart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Cart
+    {
+        private List<CartItem> cartItems = new List<CartItem>();
+
+        public void AddItem(StoreProduct storeProduct, int quantity)
+        {
+            CartItem cartItem = cartItems
+                .Where(o => o.StoreProduct.StoreProductId == storeProduct.StoreProductId)
+                .FirstOrDefault();
+
+            if (cartItem == null)
+            {
+                cartItems.Add(new CartItem
+                {
+                    StoreProduct = storeProduct,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                cartItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(StoreProduct storeProduct)
+        {
+            cartItems.RemoveAll(o => o.StoreProduct.StoreProductId == storeProduct.StoreProductId);
+        }
+
+        public decimal CalcTotalPrice()
+        {
+            return cartItems.Sum(o => o.Quantity * o.StoreProduct.Price);
+        }
+
+        public void Clear()
+        {
+            cartItems.Clear();
+        }
+
+        public IEnumerable<CartItem> Items
+        {
+            get { return cartItems; }
+        }
+    }
+
+
+
+    public class CartItem
+    {
+        public StoreProduct StoreProduct { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ SomeStore/Web/Controllers/CartController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.DbAccess;
+using Domain.Models;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGenericRepository<StoreProduct> repository;
+
+        public CartController(IGenericRepository<StoreProduct> ninjectInjectionRepository)
+        {
+            repository = ninjectInjectionRepository;
+        }
+
+        //
+        // GET: /Cart/
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int StoreProductId, string returnUrl)
+        {
+            StoreProduct product = repository.Get(StoreProductId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product,1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int StoreProductId, string returnUrl)
+        {
+            StoreProduct product = repository.Get(StoreProductId);
+
+            if (product != null)
+            {
+                GetCart().RemoveItem(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart) Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+
+
+        
+
+    }
+}
